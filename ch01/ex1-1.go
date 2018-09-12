0298c2db893d2b3ac8c84baa6dd938391746ea0a
FreeBz <freebz@hananet.net>
2018-09-12 10:38:03 +0900

    1 Go and web applications

---
+++ ch01/ex1-1.go
@@ -0,0 +1,17 @@
+// Listing 1.1  A Hello World Go web app
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
